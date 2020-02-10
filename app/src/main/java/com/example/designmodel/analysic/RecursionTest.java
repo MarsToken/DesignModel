0aa1aec3143028d7ff9569e9e4ca2b84c4cfbb2f
p_maobowang <p_maobowang@tencent.com>
2020-02-10 18:03:54 +0800

    shellRank,mergeRank,insertRank

--- app/src/main/java/com/example/designmodel/analysic/RecursionTest.java
+++ app/src/main/java/com/example/designmodel/analysic/RecursionTest.java
@@ -6,23 +6,66 @@ package com.example.designmodel.analysic;
 public class RecursionTest {
     public static void main(String[] args) {
         long currentTime1 = System.currentTimeMillis();
-        long sum = cyclic(50);//max 92
-        System.out.println("\n" + sum);
+        long sum = cyclic(40);//max 92
+        System.out.println("循环计算：" + sum);
+        System.out.println("调用次数：" + count_cyclic);
         long currentTime2 = System.currentTimeMillis();
         long result = currentTime2 - currentTime1;
-        System.out.println("Cycle result=" + result);
-        long sum2 = feiBo(50);
-        System.out.println("\n" + sum2);
+        System.out.println("Cycle result time=" + result);
+        long sum2 = feiBo(40);
+        System.out.println("递归计算：" + sum2);
+        System.out.println("调用次数：" + count);
         long currentTime3 = System.currentTimeMillis();
         long result2 = currentTime3 - currentTime2;
-        System.out.println("feiBo result=" + result2);
+        System.out.println("feiBo result time=" + result2);
+        long result3 = whileTest(40);
+        System.out.println("result3=" + result3);
     }
 
+    private static int count, count_cyclic;
+
     private static long feiBo(int n) {
-        //n=40 300ms n=50 41696ms
-        if (n > 1)
-            return feiBo(n - 1) + feiBo(n - 2);
+        count++;
+        //n=40 380ms n=50 41696ms
+//        if (n > 1)
+//            return feiBo(n - 1) + feiBo(n - 2);
+//        return n;
+
+        //n=40 270ms 减少一个方法调用 165580141次调用
+//        if (n > 1) {
+//            long result = 1;
+//            do {
+//                result += feiBo(n - 2);
+//                n--;
+//            } while (n > 1);
+//            return result;
+//        }
+//        return n;
+        //or
+        if (n > 1) {
+            int sum = 1;//sum 从n=10-2
+            while (n > 1) {
+                sum += feiBo(n - 2);
+                n--;
+            }
+            return sum;
+        }
         return n;
+
+    }
+
+    private static long whileTest(int n) {
+        int sum = 0;
+        if (n > 1) {
+            int a, b;
+            while (n > 0) {
+                n--;
+                a = n;
+                b = n - 1;
+                sum += (a + b);
+            }
+        }
+        return sum;
     }
 
     private static long cyclic(int n) {
@@ -33,6 +76,7 @@ public class RecursionTest {
             a = n & 1;
             n /= 2;
             while (n-- > 0) {
+                count_cyclic++;
                 a += b;
                 b += a;
             }
--- app/src/main/java/com/example/designmodel/rank/RankUtils.java
+++ app/src/main/java/com/example/designmodel/rank/RankUtils.java
@@ -1,5 +1,7 @@
 package com.example.designmodel.rank;
 
+import java.util.Arrays;
+
 /**
  * Created by hp on 2019/12/7.
  */
@@ -14,10 +16,13 @@ public class RankUtils {
         //bubbleSort();
         //selectSort();
         //insertSort();
+        //shellSort();
+        int[] array=mergeSort(RankUtils.array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ",");
         }
     }
+
 
     /**
      * 思路：
@@ -59,18 +64,94 @@ public class RankUtils {
         }
     }
 
+    /**
+     * 插入排序，选中a，剩下的比a大放右边，比a小放左边 用到in-place排序（即只需用到O(1)的额外空间的排序）
+     */
     private static void insertSort() {
+        if (null == array) {
+            return;
+        }
         int length = array.length;
         for (int i = 0; i < length - 1; i++) {
-            int currentIndex = i + 1;
-            for (int j = 0; j < currentIndex; j++) {
-                if (array[currentIndex] < array[j]) {
-                    array[currentIndex] = array[currentIndex] + array[j];
-                    array[j] = array[currentIndex] - array[j];
-                    array[currentIndex] = array[currentIndex] - array[j];
+            int temp_index = i + 1;
+            for (int j = 0; j < temp_index; j++) {
+                if (array[temp_index] < array[j]) {
+                    array[temp_index] += array[j];
+                    array[j] = array[temp_index] - array[j];
+                    array[temp_index] = array[temp_index] - array[j];
                 }
             }
         }
+        //误区 此时array[temp_index]并没有变，变得只是temp_value
+//        int length = array.length;
+//        for (int i = 0; i < length - 1; i++) {
+//            int temp = i + 1;
+//            int temp_value = array[temp];//如果这样的话 最后一行赋值的应该是array[temp]
+//            for (int j = 0; j < temp; j++) {
+//                if (temp_value < array[j]) {
+//                    temp_value = temp_value + array[j];
+//                    array[j] = temp_value - array[j];
+//                    temp_value = temp_value - array[j];
+//                }
+//            }
+//        }
     }
 
+    /**
+     * 插入排序的改进（又叫缩小增量排序）
+     * 它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+     */
+    private static void shellSort() {
+        if (null == array) {
+            return;
+        }
+        int length = array.length, gap = length / 2;
+        while (gap > 0) {
+            for (int i = gap; i < length - 1; i++) {
+                int temp_index = i + 1;
+                for (int j = 0; j < temp_index; j++) {
+                    if (array[temp_index] < array[j]) {
+                        array[temp_index] += array[j];
+                        array[j] = array[temp_index] - array[j];
+                        array[temp_index] = array[temp_index] - array[j];
+                    }
+                }
+            }
+            gap /= 2;
+        }
+    }
+
+    /**
+     * 归并排序
+     * 和选择排序一样，归并排序的性能不受输入数据的影响，但表现比选择排序好的多，因为始终都是O(n log n）的时间复杂度。代价是需要额外的内存空间
+     */
+    private static int[] mergeSort(int[] array) {
+        if (null == array) {
+            return array;
+        }
+        int length = array.length;
+        if (length < 2) {
+            return array;
+        }
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, length);
+        return merge(mergeSort(left), mergeSort(right));
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        System.out.println("merge left.length=" + left.length + ",merge right.length=" + right.length);
+        int[] result = new int[left.length + right.length];
+        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
+            if (i >= left.length)
+                result[index] = right[j++];
+            else if (j >= right.length)
+                result[index] = left[i++];
+            else if (left[i] > right[j])
+                result[index] = right[j++];
+            else
+                result[index] = left[i++];
+        }
+        return result;
+    }
 }
